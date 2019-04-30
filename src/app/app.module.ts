840c75fc28e0647431c879c7aba96b1186662717
rahulmnaik126 <rahulmnaik126@gmail.com>
2019-04-30 19:32:33 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } 
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
